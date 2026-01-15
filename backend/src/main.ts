f327ddc4725890a667d2d7b30c1085af0d286dbe
Mayrinova Marina <marishka.x86@gmail.com>
2026-01-15 17:25:41 +0300

    feat: добавила возможность работы с куки

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 4000);
+
+  console.log('Server running on http://localhost:4000');
 }
 bootstrap();
