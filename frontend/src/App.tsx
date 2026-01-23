0fd412219dc246a120e306c38ae75c13530de80b
Mayrinova Marina <marishka.x86@gmail.com>
2026-01-23 16:08:59 +0300

    добавила роутинг

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import logo from './logo.svg'
+import { lazy, Suspense } from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { MainPage } from './pages/MainPage'
+
+// Ленивая загрузка компонентов
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'))
+// const AuthForm = lazy(() => import('./components/AuthForm'))
+const AuthForm = lazy(() =>
+  import('./components/AuthForm').then((module) => ({
+    default: module.AuthForm,
+  }))
+)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Main,
+    children: [
+      { path: 'home', Component: HomePage },
+      { path: 'main', Component: MainPage },
+      { path: 'auth', Component: AuthForm },
+    ],
+  },
+])
+
+function Main() {
+  return (
+    <main>
+      <h1>Заголовок приложения</h1>
+      <Outlet />
+    </main>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <section className="Test">
-        <div className="login-form">
-          <div>
-            <legend> Логин</legend>
-            <input type="text" name="login" />
-          </div>
-          <div>
-            <legend> Пароль</legend>
-            <input type="password" name="password" />
-          </div>
-          <button type="submit">Войти</button>
-        </div>
-        <div>
-          <button>Продолжить с VK ID</button>
-        </div>
-      </section>
-      <input type="range" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
     </div>
   )
 }
